6032fcdadebddb55feca533254b6ffd6a9c38d91
vmahavis <vmahavis@gmail.com>
2013-10-11 10:19:55 -0400

    Added Count for Concepts

--- grails-app/services/org/transmart/search/ConceptGenCountService.groovy
+++ grails-app/services/org/transmart/search/ConceptGenCountService.groovy
@@ -5,7 +5,33 @@ import org.transmart.SearchFilter;
 class ConceptGenCountService {
 	
 	def int getCount(SearchFilter searchFilter){
-		return 92
-	}
-	
+
+        def searchText = searchFilter.searchText
+
+        StringBuffer stringBuffer = new StringBuffer();
+        try
+        {
+            u = new URL("http://conceptgen.ncibi.org/ConceptWeb/conceptservice?type=count&search=" + URLEncoder.encode(searchText, "UTF-8"));
+            BufferedReader br = new BufferedReader(new InputStreamReader(u.openStream()));
+            String line = "";
+            int lineNum = 0;
+
+            while ((line = br.readLine()) != null)
+            {
+                if (lineNum != 0)
+                {
+                    stringBuffer.append(line);
+                }
+                lineNum++;
+            }
+
+            br.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return Integer.parseInt(stringBuffer.toString());
+    }
 }
