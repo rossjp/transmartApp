4e2f001a5b26ca9deddf952ddcb0be52131978b3
Terry-Weymouth <weymouth@umich.edu>
2013-10-11 13:07:55 -0400

    minor modifications -- changed fake metscape count to return 0 for non-gene and 1 for gene

--- grails-app/services/org/transmart/search/MetscapeService.groovy
+++ grails-app/services/org/transmart/search/MetscapeService.groovy
@@ -5,6 +5,17 @@ import org.transmart.SearchFilter;
 class MetscapeService {
 	
 	def int getCount(SearchFilter searchFilter){
+		if (searchFilter == null) return 0
+		def searchText = searchFilter.searchText
+		if (searchText == null) return 0
+
+		def geneidString = SearchUtils.geneidString(searchFilter)
+		def geneidList = SearchUtils.geneidList(geneidString)
+		if (geneidList == null) return 0;
+		if (geneidList.isEmpty()) return 0;
+		
+		def geneid = geneidList[0]
+
 		return 1
 	}
 	
